4c85d26d9ab2e5f04cf6f3193613252dd713e8c2
hhj <gudwn357@gmail.com>
2022-12-21 22:05:04 +0900

    feat : create Heart Entity

---
+++ src/main/java/example/community/domain/Heart.java
@@ -0,0 +1,19 @@
+package example.community.domain;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Heart {
+
+    @Id @GeneratedValue @Column(name = "heart_id")
+    private Long id;
+}
