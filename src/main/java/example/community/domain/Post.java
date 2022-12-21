5651a29b3ea320b3c824785f113a71f44554b615
hhj <gudwn357@gmail.com>
2022-12-21 22:03:30 +0900

    feat : create Post Entity

---
+++ src/main/java/example/community/domain/Post.java
@@ -0,0 +1,24 @@
+package example.community.domain;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Post {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "post_id")
+    private Long id;
+
+    private String title;
+    private String body;
+}
