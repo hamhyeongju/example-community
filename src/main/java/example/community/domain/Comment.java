4ef98d4da6da792be744a18e2c6268385cf60a0b
hhj <gudwn357@gmail.com>
2022-12-21 22:04:13 +0900

    feat : create Comment Entity

---
+++ src/main/java/example/community/domain/Comment.java
@@ -0,0 +1,23 @@
+package example.community.domain;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Comment {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "comment_id")
+    private Long id;
+
+    private String body;
+}
