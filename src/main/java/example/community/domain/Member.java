7f63dde5bc8305b373745783e22bd114b5670f14
hhj <gudwn357@gmail.com>
2022-12-21 22:02:55 +0900

    feat : create Member Entity

---
+++ src/main/java/example/community/domain/Member.java
@@ -0,0 +1,25 @@
+package example.community.domain;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    private String loginId;
+    private String password;
+    private String name;
+}
