00169528a8109fddf855aed76a2b034f646a402c
hhj <gudwn357@gmail.com>
2022-12-22 22:23:24 +0900

    feat (PostService) : create findList()

--- src/main/java/example/community/service/PostService.java
+++ src/main/java/example/community/service/PostService.java
@@ -1,11 +1,27 @@
 package example.community.service;
 
+import example.community.domain.Post;
+import example.community.repository.PostRepository;
+import example.community.service.dto.PostListDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class PostService {
+
+    private final PostRepository postRepository;
+
+    public List<PostListDto> findList() {
+        List<Post> find = postRepository.findPostList();
+        return find.stream().map(post -> {
+            return new PostListDto(post.getId(), post.getTitle(), post.getMember().getName(),
+                    post.getHeartNum(), post.getCommentNum());
+        }).collect(Collectors.toList());
+    }
 }
