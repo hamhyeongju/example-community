16643db77842ed07806fa81db93bb5b21e5e54e3
hhj <gudwn357@gmail.com>
2022-12-22 22:34:34 +0900

    feat (PostService) : create findPostAndComment()

--- src/main/java/example/community/service/PostService.java
+++ src/main/java/example/community/service/PostService.java
@@ -2,6 +2,8 @@ package example.community.service;
 
 import example.community.domain.Post;
 import example.community.repository.PostRepository;
+import example.community.service.dto.CommentDto;
+import example.community.service.dto.PostDto;
 import example.community.service.dto.PostListDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -23,5 +25,15 @@ public class PostService {
             return new PostListDto(post.getId(), post.getTitle(), post.getMember().getName(),
                     post.getHeartNum(), post.getCommentNum());
         }).collect(Collectors.toList());
+    }
+
+    public PostDto findPostAndComment(Long post_id) {
+        Post findPost = postRepository.findById(post_id).orElseThrow(IllegalAccessError::new);
+
+        List<CommentDto> commentDtos = findPost.getComments().stream().map(comment -> {
+            return new CommentDto(comment.getId(), comment.getBody(), comment.getMember().getName());
+        }).collect(Collectors.toList());
+
+        return new PostDto(findPost.getId(), findPost.getTitle(), findPost.getBody(), findPost.getMember().getName(), commentDtos);
     }
 }
