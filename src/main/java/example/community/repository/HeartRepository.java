6f9c4d87fac28a1b29617a507331707a52daa998
hhj <gudwn357@gmail.com>
2022-12-24 18:55:38 +0900

    feat (HeartRepository) : create findByMemberIdAndPostId using @Query

--- src/main/java/example/community/repository/HeartRepository.java
+++ src/main/java/example/community/repository/HeartRepository.java
@@ -2,6 +2,13 @@ package example.community.repository;
 
 import example.community.domain.Heart;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface HeartRepository extends JpaRepository<Heart, Long> {
+
+    @Query("select h from Heart h where h.post.id = : post_id and  h.member.id = :member_id")
+    Optional<Heart> findByMemberIdAndPostId(@Param("post_id") Long post_id, @Param("member_id") Long member_id);
 }
