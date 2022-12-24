cb6dbb02efa271b3cebc91a77564ae6a1c0065db
hhj <gudwn357@gmail.com>
2022-12-24 19:19:43 +0900

    feat : create CommentController

---
+++ src/main/java/example/community/controller/CommentController.java
@@ -0,0 +1,21 @@
+package example.community.controller;
+
+import example.community.configuration.security.UserDetailsImpl;
+import example.community.service.CommentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+@RequiredArgsConstructor
+public class CommentController {
+
+    private final CommentService commentService;
+
+
+
+}
