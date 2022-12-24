012ca595d25aa42b04e5a32ed4b834fd4795d049
hhj <gudwn357@gmail.com>
2022-12-24 18:43:17 +0900

    feat (HeartService) : create changeHearteStatus()

--- src/main/java/example/community/service/HeartService.java
+++ src/main/java/example/community/service/HeartService.java
@@ -1,5 +1,11 @@
 package example.community.service;
 
+import example.community.domain.Heart;
+import example.community.domain.Member;
+import example.community.domain.Post;
+import example.community.repository.HeartRepository;
+import example.community.repository.MemberRepository;
+import example.community.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -8,4 +14,24 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class HeartService {
+
+    private final PostRepository postRepository;
+    private final MemberRepository memberRepository;
+    private final HeartRepository heartRepository;
+
+    public void changeHeartStatus(Long post_id, Long member_id) {
+
+        heartRepository.findByMemberIdAndPostId(post_id, member_id).ifPresentOrElse(
+                heart -> {
+                    heartRepository.delete(heart);
+                    heart.getPost().minusHeartNum();
+                },
+                () -> {
+                    Post post = postRepository.findById(post_id).orElseThrow(IllegalAccessError::new);
+                    Member member = memberRepository.findById(member_id).orElseThrow(IllegalAccessError::new);
+
+                    heartRepository.save(Heart.createHeart(post, member));
+                    post.plusHeartNum();
+                });
+    }
 }
