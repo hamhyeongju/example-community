3685b247a10b45e0f1db5027269980431d86ef03
hhj <gudwn357@gmail.com>
2022-12-25 17:09:27 +0900

    feat (MemberController) : create joinForm(), loginForm()

--- src/main/java/example/community/controller/MemberController.java
+++ src/main/java/example/community/controller/MemberController.java
@@ -2,12 +2,33 @@ package example.community.controller;
 
 import example.community.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Controller
 @RequiredArgsConstructor
 public class MemberController {
 
     private final MemberService memberService;
+
+    /**
+     * 회원가입 폼
+     */
+    @GetMapping("/join")
+    public String joinForm(@ModelAttribute("memberDto") MemberDto memberDto) {
+        return "member/join";
+    }
+
+    /**
+     * 로그인 폼
+     */
+    @GetMapping("/login")
+    public String loginForm(@ModelAttribute("loginDto") LoginDto loginDto,
+                            @AuthenticationPrincipal UserDetails userDetails) {
+        return (userDetails == null) ? "member/login" : "redirect:/";
+    }
 
 }
