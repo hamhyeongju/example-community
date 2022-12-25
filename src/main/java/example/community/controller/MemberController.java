fc1835934cb22513bc6a3eb32d01c07ae022f7f4
hhj <gudwn357@gmail.com>
2022-12-25 17:02:55 +0900

    feat : create MemberController

---
+++ src/main/java/example/community/controller/MemberController.java
@@ -0,0 +1,13 @@
+package example.community.controller;
+
+import example.community.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+}
