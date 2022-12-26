caedac1b87c0e1b3df3d0b634a6fea272cfcef1e
hhj <gudwn357@gmail.com>
2022-12-26 18:28:44 +0900

    feat (Configurer) : add @Bean BCryptPasswordEncoder

--- src/main/java/example/community/configuration/Configurer.java
+++ src/main/java/example/community/configuration/Configurer.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -26,5 +27,10 @@ public class Configurer {
                 .logoutSuccessUrl("/");
 
         return http.build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
