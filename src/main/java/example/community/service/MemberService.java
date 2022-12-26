24d9843e7a0a97b37c1d14e4cce24e0e9a7503ce
hhj <gudwn357@gmail.com>
2022-12-26 18:03:30 +0900

    feat (MemberService) : create delete()

--- src/main/java/example/community/service/MemberService.java
+++ src/main/java/example/community/service/MemberService.java
@@ -1,11 +1,15 @@
 package example.community.service;
 
+import example.community.domain.Comment;
+import example.community.domain.Heart;
 import example.community.domain.Member;
 import example.community.repository.MemberRepository;
 import example.community.service.dto.MemberJoinDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @Transactional(readOnly = true)
@@ -16,5 +20,17 @@ public class MemberService {
 
     public void save(MemberJoinDto memberJoinDto) {
         memberRepository.save(Member.createMember(memberJoinDto.getLoginId(), memberJoinDto.getPassword(), memberJoinDto.getName().strip()));
+    }
+
+    public void delete(Long member_id) {
+        Member member = memberRepository.findById(member_id).orElseThrow(IllegalAccessError::new);
+
+        List<Comment> comments = member.getComments();
+        comments.forEach(comment -> comment.getPost().minusCommentNum());
+
+        List<Heart> hearts = member.getHearts();
+        hearts.forEach(like -> like.getPost().minusHeartNum());
+
+        memberRepository.delete(member);
     }
 }
