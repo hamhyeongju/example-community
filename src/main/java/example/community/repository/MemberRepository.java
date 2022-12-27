0b2f15cbe312369fa346f6d35c2560875f81fa1e
hhj <gudwn357@gmail.com>
2022-12-27 17:31:38 +0900

    feat (MemberRepository) : create findByLoginId()

--- src/main/java/example/community/repository/MemberRepository.java
+++ src/main/java/example/community/repository/MemberRepository.java
@@ -3,5 +3,9 @@ package example.community.repository;
 import example.community.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByLoginId(String loginId);
 }
