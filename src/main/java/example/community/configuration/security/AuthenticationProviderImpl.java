76a4db2da774e2255af232b2e73bd04e5dff749c
hhj <gudwn357@gmail.com>
2022-12-27 17:25:27 +0900

    feat : create AuthenticationProviderImpl

---
+++ src/main/java/example/community/configuration/security/AuthenticationProviderImpl.java
@@ -0,0 +1,41 @@
+package example.community.configuration.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class AuthenticationProviderImpl implements AuthenticationProvider {
+
+    private final UserDetailsService service;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+
+        UsernamePasswordAuthenticationToken token = (UsernamePasswordAuthenticationToken) authentication;
+        String username = token.getName();
+        String password = (String) token.getCredentials();
+
+        UserDetailsImpl loginMember = (UserDetailsImpl) service.loadUserByUsername(username);
+
+        if (!bCryptPasswordEncoder.matches(password, loginMember.getPassword())) {
+            throw new BadCredentialsException(loginMember.getUsername() + " 비밀번호를 확인해주세요.");
+        }
+
+        return new UsernamePasswordAuthenticationToken(loginMember, password, loginMember.getAuthorities());
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return authentication.equals(UsernamePasswordAuthenticationToken.class);
+    }
+}
