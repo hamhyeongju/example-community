6fcb08b413d6fdccbd0377a31a638884988e5952
hhj <gudwn357@gmail.com>
2022-12-31 19:00:50 +0900

    feat (Configurer) : add PostInterceptor

--- src/main/java/example/community/configuration/Configurer.java
+++ src/main/java/example/community/configuration/Configurer.java
@@ -1,15 +1,22 @@
 package example.community.configuration;
 
+import example.community.configuration.interceptor.PostInterceptor;
+import example.community.domain.Post;
+import example.community.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @RequiredArgsConstructor
-public class Configurer {
+public class Configurer implements WebMvcConfigurer {
+
+    private final PostService postService;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -32,5 +39,13 @@ public class Configurer {
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new PostInterceptor(postService))
+                .order(1)
+                .excludePathPatterns("/post/add")
+                .addPathPatterns("/post/edit/{post_id}", "/post/{post_id}");
     }
 }
