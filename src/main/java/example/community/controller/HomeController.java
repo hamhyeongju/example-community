bf5d597b8bf9cc526239d3b196dc15729af365ef
hhj <gudwn357@gmail.com>
2023-01-03 17:46:16 +0900

    feat (home.html) : add 회원 탈퇴 button

--- src/main/java/example/community/controller/HomeController.java
+++ src/main/java/example/community/controller/HomeController.java
@@ -1,7 +1,10 @@
 package example.community.controller;
 
+import example.community.configuration.security.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -9,7 +12,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
     @GetMapping("/")
-    public String home() {
+    public String home(@AuthenticationPrincipal UserDetailsImpl userDetails, Model model) {
+        if (userDetails != null) model.addAttribute("member_id", userDetails.getMember().getId());
+
         return "home/home";
     }
 }
